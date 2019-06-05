fbe26ba6c5eb909c2b5c3ac7aaab56a87cd55143
Neil McHenry <neilmchenry@gmail.com>
2019-06-04 20:50:19 -0500

    Added roboMap .umap

---
+++ Source/robocoop/robocoopGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "robocoopGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOCOOP_API ArobocoopGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
